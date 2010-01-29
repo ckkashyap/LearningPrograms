811443623facbcf246a745074ea851806bca7a48
kashyap <kashyap@trinity318.(none)>
2010-01-29 21:28:48 +0530

    Added the bottumUp  function from the net

--- Haskell/TrianglePath.hs
+++ Haskell/TrianglePath.hs
@@ -22,8 +22,20 @@ maxPath1 list = maxPath' list 0 0 [0]
 			a = maxPath' xs c (s+x!!c) (0:l)
 			b = maxPath' xs (c+1) (s+x!!c) (1:l)
 							   
-
+bottomUp :: (Ord a, Num a) => [[a]] -> a
+bottomUp = head . bu
+  where bu [bottom]     = bottom
+        bu (row : base) = merge row $ bu base
+        merge [] [_] = []
+        merge (x:xs) (y1:y2:ys) = x + max y1 y2 : merge xs (y2:ys)
 
 m = maxPath1 [[1],[2,1],[1,2,3]]
 n = maxPath1 [[1],[1,2],[4,1,2],[2,3,1,1]]
 o = maxPath1 [[1],[2,1]]
+
+x=bottomUp [[1],[2,1],[1,2,3]]
+
+
+mrg [] [_] = []
+mrg (x:xs) (y1:y2:ys) = x + max y1 y2 : mrg xs (y2:ys)
+
