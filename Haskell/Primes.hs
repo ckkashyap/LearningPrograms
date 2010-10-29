8d161c6b0c3a9b3c3b2e353b2fd2e466455047be
Kashyap K Chatamballi <ckk@croudwear-lm.eglbp.corp.yahoo.com>
2010-10-29 09:45:08 +0530

    another prime

--- Haskell/Primes.hs
+++ Haskell/Primes.hs
@@ -1,3 +1,11 @@
-primes = f [2..]
+primes = f [2,3..]
 	where
 		f (p:xs) = p : f [ x | x <- xs, x `mod` p /= 0]
+
+
+
+primes1 :: Integral a => [a]
+primes1 = 2 : filter isPrime [3..]
+
+isPrime :: Integral a => a -> Bool
+isPrime n = all (\x -> mod n x /= 0) . takeWhile (\x -> x*x <= n) $ primes1
