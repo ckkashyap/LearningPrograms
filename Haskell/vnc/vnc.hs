d7c33b1fa14c00f432ab70c70820d0e2de8f132f
Kashyap <ckkashyap@gmail.com>
2010-11-04 11:58:49 +0530

    working a bit

--- Haskell/vnc/vnc.hs
+++ Haskell/vnc/vnc.hs
@@ -10,50 +10,47 @@ import Data.Binary.Get
 import Data.Word
 
 
-main2 :: IO ()
-main2 = do
+main :: IO ()
+main = do
 	running <- serveOne (Just $ UserWithDefaultGroup "ckk") server
 	putStrLn "server is accepting connections!!!"
 	waitFor running
 
-	where server = Server (SockAddrInet 5901 iNADDR_ANY) Stream echo
-
-main :: IO ()
-main = main2
-
-deserializeHeader :: Get (Word8, Word16, Word32)
-deserializeHeader = do
-  a <- getWord8
-  b <- getWord16le
-  c <- getWord32le
-  return (a,b,c)
+	where server = Server (SockAddrInet 5901 iNADDR_ANY) Stream doVNC
 
 
 
+byteString2Number :: BS.ByteString -> Int
+byteString2Number bs = _byteString2Number 1 (digits bs)
+	where
+		_byteString2Number _ [] = 0
+		_byteString2Number n (x:xs) = (n*x) + (_byteString2Number (n*10) xs)
+		digits bs = map ((+(-48)).fromIntegral) (BS.unpack(BS.reverse bs))
 
---startRFB :: GHC.IO.Handle.Types.Handle -> IO ()
+
+readClientHeader  = do
+	getLazyByteString 4
+	m <- getLazyByteString 3 
+	getWord8
+	n <- getLazyByteString 3
+	getWord8
+	let majorVersionNumber = byteString2Number m
+	let minorVersionNumber = byteString2Number n
+	if (majorVersionNumber /= 3) then 
+		fail ("ERROR: Unsupported version " ++ (show majorVersionNumber))
+		else 
+		return (byteString2Number m,byteString2Number n)
+
+startRFB :: Handle -> IO ()
 startRFB h = do
 		hPutStrLn h "RFB 003.003"
 		hFlush h
 		x <- BS.hGet h 12
-		let (aa,bb,cc)=(runGet deserializeHeader x)
-		putStrLn (show aa) 
-		putStrLn (show bb)
+		let (m,n) = ( runGet readClientHeader x)
+		hPutStrLn h (show m)
+		hFlush h
 		return ()
 			
-
-
 -- |the simple echo server routine
-echo :: ServerRoutine
-echo (h,n,p) = do
-			startRFB h
-
-{-
-	cnt <- BS.hGetContents h
-	let (a,b,c) = ( runGet deserializeHeader cnt)
-	hPutStrLn h (show a)
-	hFlush h
-
-	--when ((l!!0)/='q') (echo (h,n,p))
-
--}
+doVNC :: ServerRoutine
+doVNC (h,n,p) = do startRFB h
