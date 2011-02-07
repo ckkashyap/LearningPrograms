65dde044f6d8298849d69a918bffb5bff43d2dc9
Kashyap <ckkashyap@gmail.com>
2011-02-07 14:30:01 +0530

    forking IO

---
+++ Haskell/forkIO.hs
@@ -0,0 +1,27 @@
+import Data.IORef
+import Control.Concurrent
+import Control.Monad
+
+startThread ref = forever $ do
+			msg <- readIORef ref
+			putStrLn (show msg )
+			threadDelay 1000000
+			return ()
+
+
+{-
+croudwear-lm:Haskell ckk$ ghci forkIO.hs 
+GHCi, version 6.12.3: http://www.haskell.org/ghc/  :? for help
+Loading package ghc-prim ... linking ... done.
+Loading package integer-gmp ... linking ... done.
+Loading package base ... linking ... done.
+Loading package ffi-1.0 ... linking ... done.
+[1 of 1] Compiling Main             ( forkIO.hs, interpreted )
+Ok, modules loaded: Main.
+*Main> ref <- newIORef "Hello"
+*Main> t<-forkIO (startThread ref)
+"Hello"
+*Main> "Hello"
+-}
+
+
