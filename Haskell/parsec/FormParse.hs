2727cb2c8176d03229574394d457622d97d0de08
ck <ck@ck-desktop.(none)>
2011-03-02 11:29:40 +0530

    Working version of expression parsing

--- Haskell/parsec/FormParse.hs
+++ Haskell/parsec/FormParse.hs
@@ -1,72 +1,52 @@
-import Control.Monad (ap, liftM2)
 import Text.ParserCombinators.Parsec
 import Numeric (readHex)
-
-queryParser :: CharParser () [(String, Maybe String)]
-queryParser = pairParser `sepBy` char '&'
-
-pairParser :: CharParser () (String, Maybe String)
-pairParser = do
-  name <- many1 charParser
-  value <- optionMaybe (char '=' >> many charParser)
-  return (name, value)
-
-charParser :: CharParser () Char
-charParser = oneOf urlBaseChars
-     <|> (char '+' >> return ' ')
-     <|> hexParser
-
-urlBaseChars = ['a'..'z']++['A'..'Z']++['0'..'9']++"$-_.!*'(),"
-
-hexParser :: CharParser () Char
-hexParser = do
-  char '%'
-  a <- hexDigit
-  b <- hexDigit
-  let ((d, _):_) = readHex [a,b]
-  return . toEnum $ d
-
-
-
-
 
 data Tree a = Nil | Node a (Tree a ) (Tree a) deriving (Show)
 
 data Element = Number Int | Plus | Minus | Multiply | Divide deriving (Show)
 
+type AST = Tree Element
 
 
-expressionParser :: CharParser () (Tree Element)
-expressionParser = 
-	do
-		n1 <- numParser
-		(Node o _ _) <- operatorParser
-		n2 <- numParser
-		return (Node o n1 n2)
+parseAST :: CharParser () AST
+parseAST = parseAdd
+
+parseAdd :: CharParser () AST
+parseAdd = do
+	e1 <- parseMul
+	(rest e1) <|> return e1
+	where
+		rest e1 = do
+			string "+"
+			e2 <- parseAdd
+			return (Node Plus e1 e2)
+
+parseMul :: CharParser () AST
+parseMul = do
+	e1 <- parseBase
+	(rest e1) <|> return e1
+	where
+		rest e1 = do
+			string "*"
+			e2 <- parseMul
+			return (Node Multiply e1 e2)
+
+parseBase :: CharParser () AST
+parseBase = do
+	withParens <|> numParser
+	where
+		withParens = do
+				string "("
+				e <- parseAST
+				string ")"
+				return e
+
+
+parsePlus :: CharParser () AST
+parsePlus = do
+	return (Node (Number 1) Nil Nil)
 
 numParser :: CharParser () (Tree Element)
 numParser = do
 	c <- many1 (oneOf "1234567890")
 	return (Node (Number (read c:: Int)) Nil Nil)
-
-
-operatorParser :: CharParser () (Tree Element)
-operatorParser =
-		do 
-			char '+'
-			return (Node Plus Nil Nil)
-	<|> do 
-		char '-'
-		return (Node Minus Nil Nil)
-	<|> do 
-		char '*'
-		return (Node Multiply Nil Nil)
-	<|> do 
-		char '/'
-		return (Node Divide Nil Nil)
-
-	
-
-
-
-
