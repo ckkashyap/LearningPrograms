a89007f410f27b3ffd92f4f9e4a1855d1d0aeca0
Kashyap <ckkashyap@gmail.com>
2011-03-11 16:09:30 +0530

    bytestring

---
+++ Haskell/binary/MyByteString.hs
@@ -0,0 +1,36 @@
+import Data.Bits ((.|.), (.&.), shiftL, shiftR)
+import qualified Data.ByteString as B
+import Data.ByteString.Internal (ByteString(..), mallocByteString, memcpy,
+                                 unsafeCreate)
+import Data.ByteString.Unsafe (unsafeIndex)
+import Data.Word (Word8, Word16, Word32)
+import Foreign.ForeignPtr (ForeignPtr, mallocForeignPtrArray, withForeignPtr)
+import Foreign.Ptr (Ptr, castPtr, minusPtr, plusPtr)
+import Foreign.Storable (peek, peekElemOff, poke, pokeElemOff)
+import System.IO.Unsafe (unsafePerformIO)
+
+
+input = B.pack $ [1..100]
+
+f :: ByteString -> ByteString
+f bs@(PS sfp soff slen) = unsafePerformIO $ do
+	dfp <- mallocByteString slen
+	withForeignPtr dfp $ \ptr -> do
+		let p0 = plusPtr ptr 0 :: Ptr Word8
+		let p1 = plusPtr ptr 1 :: Ptr Word8
+		let p2 = plusPtr ptr 2 :: Ptr Word8
+		let p3 = plusPtr ptr 3 :: Ptr Word8
+		let p4 = plusPtr ptr 4 :: Ptr Word8
+		poke p0 65 
+		poke p1 66 
+		poke p2 67 
+		poke p3 68 
+		poke p4 69 
+	return $! PS dfp 0 slen
+	
+
+output = ""
+
+main = do
+	putStrLn output
+	
