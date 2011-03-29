f27478ddf498389183479627043be2b963c38f5d
Kashyap <ckkashyap@gmail.com>
2011-03-29 17:43:09 +0530

    checkpoint

--- perl/ps/main.pl
+++ perl/ps/main.pl
@@ -1,11 +1,11 @@
-use PS;
+use Graph;
 
 
-my $x=new PS;
+my $x=new Graph;
 
-$x->line(10,10,100,100,1,0,0);
-$x->print("Hello\nWorld", 50,50,0,1,0);
-$x->vprint("Hello\nWorld", 100,100,0,0,1);
-$x->line(100,100,100,200,0,0,1);
-$x->save;
+$x->insertValue("qps",50,100);
+$x->insertValue("qps",60,250);
+$x->insertValue("qps",70,300);
+$x->insertValue("qps",80,200);
 
+$x->save("hello.ps");
