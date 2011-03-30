166db4ec487c211354fe91c5f111d91075c86a75
Kashyap <ckkashyap@gmail.com>
2011-03-30 20:28:39 +0530

    util function

---
+++ Haskell/apps/Util.hs
@@ -0,0 +1,15 @@
+module Util where
+
+split :: Eq a => [a] -> a -> [[a]]
+split [] _ = []
+split cs c = first:rest
+	where
+		(first,n) = fetchToken cs c
+		rest = split (drop n cs) c
+
+fetchToken [] _ = ([],0)
+fetchToken (x:xs) c
+	| c == x = ([],1)
+	| otherwise = (x:word,n+1)
+		where
+			(word,n) = fetchToken xs c
