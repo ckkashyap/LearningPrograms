947ad2613120f07a6b2a75fddf0fd0bfd79deaaa
CK Kashyap <ckkashyap@gmail.com>
2011-04-17 14:32:26 +0530

    update

--- Haskell/deflate/huffman.hs
+++ Haskell/deflate/huffman.hs
@@ -13,7 +13,6 @@ input = [2,1,3,3]
 bl_count :: CodeLengths -> Array.Array CodeLength Count
 bl_count l = bl_count' l arr
 	where
-<<<<<<< HEAD
 		arr = Array.array (0,max) [(i,0) | i <- [0..max]]
 		max = (head.reverse.sort) l
 		bl_count' [] a = a
@@ -25,31 +24,6 @@ bl_count l = bl_count' l arr
 
 
 
-=======
-		newMap	= case val of
-			Just i	-> insert (i+1)
-			_	-> insert 1
-		val	= Map.lookup x m
-		insert n= Map.insert x n m
-
-
-test_bl_count = bl_count (Map.insert 0 0 Map.empty) input
-
-maxBits :: Ord a => [a] -> a
-maxBits = head.reverse.sort
-
-----------------------------
-
-code 0 = 0 
-code i = ((code (i-1) + bitCount (i-1)) `shiftL` 1)
-	where
-		bitCount n = let v = Map.lookup n test_bl_count in
-				case v of
-					Just i	-> i
-					_	-> 0
-			
-
->>>>>>> a505d0afe90a30dfeceb6f648dda67031118d7c0
 
 ------------------------------
 
