c84c390508069992850e361fe825aa63f7a3e09d
CK Kashyap <ckkashyap@gmail.com>
2011-04-17 14:27:26 +0530

    Map to array

--- Haskell/deflate/huffman.hs
+++ Haskell/deflate/huffman.hs
@@ -7,37 +7,30 @@ type CodeLength = Int
 type CodeLengths = [CodeLength]
 type Count = Int
 
-type CodeLengthCounts = Map.Map CodeLength Count
-
 input :: CodeLengths
 input = [2,1,3,3]
 
-bl_count :: CodeLengthCounts -> CodeLengths -> CodeLengthCounts
-bl_count m [] = m
-bl_count m (x:xs) = bl_count newMap xs
+bl_count :: CodeLengths -> Array.Array CodeLength Count
+bl_count l = bl_count' l arr
 	where
-		newMap	= case val of
-			Just i	-> insert (i+1)
-			_	-> insert 1
-		val	= Map.lookup x m
-		insert n= Map.insert x n m
+		arr = Array.array (0,max) [(i,0) | i <- [0..max]]
+		max = (head.reverse.sort) l
+		bl_count' [] a = a
+		bl_count' (x:xs) a = bl_count' xs na
+			where
+				na = a Array.// [(x,v+1)]
+				v = a Array.! x
+		
 
 
-test_bl_count = bl_count (Map.insert 0 0 Map.empty) input
 
-maxBits :: Ord a => [a] -> a
-maxBits = head.reverse.sort
 
-----------------------------
+------------------------------
 
-code 0 = 0 
-code i = ((code (i-1) + bitCount (i-1)) `shiftL` 1)
+minCode' bc 0 = 0 
+minCode' bc i = ((minCode' bc (i-1) + bitCount (i-1)) `shiftL` 1)
 	where
-		bitCount n = let v = Map.lookup n test_bl_count in
-				case v of
-					Just i	-> i
-					_	-> 0
+		bitCount n = bc  Array.! n
 			
-
-
+minCode = minCode' (bl_count input)
 
