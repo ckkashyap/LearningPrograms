c9c26b85dcd461fa5c188a67fa84d8e1f155e286
Kashyap <ckkashyap@gmail.com>
2011-06-30 15:43:34 +0530

    my brain

---
+++ Kashyap.mm
@@ -0,0 +1,73 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1293196993166" ID="Freemind_Link_1219610603" MODIFIED="1293197097089" TEXT="Kashyap">
+<node CREATED="1293197135954" FOLDED="true" ID="Freemind_Link_976450516" MODIFIED="1293197146309" POSITION="left" TEXT="Haskell">
+<node CREATED="1293197043350" FOLDED="true" ID="_" MODIFIED="1293197048660" TEXT="Monads">
+<node CREATED="1293197049240" ID="Freemind_Link_1187501215" MODIFIED="1293197054163" TEXT="Monad Transformers"/>
+<node CREATED="1293197061872" ID="Freemind_Link_1400122266" MODIFIED="1293197064506" TEXT="Videos">
+<node CREATED="1293197297548" ID="Freemind_Link_1052675807" MODIFIED="1293197298967" TEXT="http://blip.tv/file/3461134/"/>
+</node>
+<node CREATED="1293197181823" ID="Freemind_Link_1212142836" MODIFIED="1293197216193" TEXT="Monad transformers">
+<node CREATED="1293197216534" ID="Freemind_Link_1101161348" MODIFIED="1293197218161" TEXT="http://en.wikibooks.org/wiki/Haskell/Monad_transformers"/>
+</node>
+</node>
+<node CREATED="1293197174080" ID="Freemind_Link_1650064962" MODIFIED="1293197180251" TEXT="Scheme in 48 hours"/>
+<node CREATED="1293197224405" ID="Freemind_Link_1127367441" MODIFIED="1293197231608" TEXT="VNC graphics"/>
+<node CREATED="1293197251174" ID="Freemind_Link_1040462252" MODIFIED="1293197255786" TEXT="Source indexing">
+<node CREATED="1293197256173" ID="Freemind_Link_1225847843" MODIFIED="1293197263057" TEXT="parsing computer programs"/>
+</node>
+</node>
+<node CREATED="1293197317354" FOLDED="true" ID="Freemind_Link_1276991111" MODIFIED="1293197322013" POSITION="right" TEXT="VI Tips">
+<node CREATED="1293197324993" FOLDED="true" ID="Freemind_Link_1611119688" MODIFIED="1293197350222" TEXT="counter stuff using let ">
+<node COLOR="#cc6600" CREATED="1293197641415" ID="Freemind_Link_264815522" MODIFIED="1293197764092" STYLE="fork" TEXT=":let ct=1&#xa;:g#\(.*\)#s//\=ct.&apos; &apos;.submatch(0)/|let ct+=1"/>
+</node>
+</node>
+<node CREATED="1293201516333" FOLDED="true" ID="Freemind_Link_1166184572" MODIFIED="1293201518360" POSITION="left" TEXT="MATHS">
+<node CREATED="1293201518659" ID="Freemind_Link_1242538364" MODIFIED="1293201528655" TEXT="Perumatations from book on HD"/>
+<node CREATED="1293201536155" ID="Freemind_Link_449759541" MODIFIED="1293201543518" TEXT="queuing theory">
+<node CREATED="1309428352432" ID="Freemind_Link_1368704796" MODIFIED="1309428357436" TEXT="From&apos;s rishi&apos;s book"/>
+</node>
+<node CREATED="1309428345920" ID="Freemind_Link_47137946" MODIFIED="1309428349900" TEXT="Linear algebra"/>
+<node CREATED="1309428418866" ID="Freemind_Link_1371428118" MODIFIED="1309428427997" TEXT="Category theory for the working math"/>
+</node>
+<node CREATED="1309428285751" FOLDED="true" ID="Freemind_Link_675126247" MODIFIED="1309428288355" POSITION="right" TEXT="EDSL">
+<node CREATED="1309428288839" ID="Freemind_Link_178060780" MODIFIED="1309428294946" TEXT="Follow up with Dave&apos;s stuff"/>
+<node CREATED="1309428296439" ID="Freemind_Link_1649638091" MODIFIED="1309428304666" TEXT="Dave has sent the Haskell files">
+<node CREATED="1309428308152" ID="Freemind_Link_1873702691" MODIFIED="1309428314739" TEXT="They are lhs files">
+<node CREATED="1309428315135" ID="Freemind_Link_1879179293" MODIFIED="1309428318067" TEXT="Need to clean up"/>
+</node>
+</node>
+</node>
+<node CREATED="1309428331279" FOLDED="true" ID="Freemind_Link_1971745869" MODIFIED="1309428333179" POSITION="left" TEXT="EMACS">
+<node CREATED="1309428334015" ID="Freemind_Link_1842950409" MODIFIED="1309428339492" TEXT="Go over the tutorial"/>
+</node>
+<node CREATED="1309428441202" ID="Freemind_Link_1396240679" MODIFIED="1309428444661" POSITION="right" TEXT="Presentation">
+<node CREATED="1309428445674" ID="Freemind_Link_193254249" MODIFIED="1309428452477" TEXT="FP"/>
+<node CREATED="1309428459307" ID="Freemind_Link_1971195930" MODIFIED="1309428463231" TEXT="Power of languages">
+<node CREATED="1309428467971" ID="Freemind_Link_1304837898" MODIFIED="1309428474934" TEXT="Ofcourse reference to the blub paradox"/>
+</node>
+<node CREATED="1309428510443" ID="Freemind_Link_1627706775" MODIFIED="1309428514159" TEXT="The expression problem"/>
+<node CREATED="1309428526397" ID="Freemind_Link_537623892" MODIFIED="1309428531463" TEXT="low level vs high level">
+<node CREATED="1309428531820" ID="Freemind_Link_1063925370" MODIFIED="1309428548848" TEXT="tuning the MPFI  of a car vs looking for a good route"/>
+<node CREATED="1309428553085" ID="Freemind_Link_289896470" MODIFIED="1309428560776" TEXT="Problem domain vs the programming domain"/>
+</node>
+<node CREATED="1309428671719" ID="Freemind_Link_1269145076" MODIFIED="1309428708268" TEXT="Which is the best language">
+<node CREATED="1309428677704" ID="Freemind_Link_156749086" MODIFIED="1309428687636" TEXT="Language written for the problem">
+<node CREATED="1309428688008" ID="Freemind_Link_1140711979" MODIFIED="1309428689428" TEXT="DSL"/>
+</node>
+</node>
+</node>
+<node CREATED="1309428478931" ID="Freemind_Link_167808535" MODIFIED="1309428482031" POSITION="right" TEXT="Scala"/>
+<node CREATED="1309428483787" ID="Freemind_Link_356586221" MODIFIED="1309428488007" POSITION="left" TEXT="Lift framework"/>
+<node CREATED="1309428491860" ID="Freemind_Link_1540716460" MODIFIED="1309428500311" POSITION="right" TEXT="Java PC"/>
+<node CREATED="1309428584991" ID="Freemind_Link_1533984227" MODIFIED="1309428586418" POSITION="left" TEXT="Links">
+<node CREATED="1309428649695" FOLDED="true" ID="Freemind_Link_1850302310" MODIFIED="1309428651227" TEXT="Books">
+<node CREATED="1309428587071" FOLDED="true" ID="Freemind_Link_577927364" MODIFIED="1309428631732" TEXT="Chinese link">
+<node CREATED="1309428632751" ID="Freemind_Link_607350024" MODIFIED="1309428634599" TEXT="http://ishare.iask.sina.com.cn/f/10129928.html">
+<node CREATED="1309428635344" ID="Freemind_Link_1590967457" MODIFIED="1309428638891" TEXT="ckkashyap/Bri.."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
