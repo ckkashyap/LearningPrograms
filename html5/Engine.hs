d4f2beea7032a4b6786fe508ddfcecd9a1e1133b
U-ADOBENET\kck <kck@kck-W7-2.corp.adobe.com>
2012-01-01 14:10:04 +0530

    working version

--- html5/Engine.hs
+++ html5/Engine.hs
@@ -16,7 +16,8 @@ rotate :: Point3D -> Angle -> Axis -> Point3D
 rotate (x,y,z) angle axis = (x', y', z')
        where
         (x',y',z') = case axis of
-           X -> (x, f1 y z, f2 y z)
+--           X -> (x, (f1 y (z + zCoordinate)) - zCoordinate, (f2 y (z + zCoordinate)) - zCoordinate)
+           X -> (x, (f1 y z), (f2 y z))
            Y -> (f2 z x, y, f1 z x)
            Z -> (f1 x y, f2 x y, z) 
         f1 a b = (a * cos rads) - (b * sin rads)
@@ -36,20 +37,26 @@ to2DSegment :: LineSegment3D -> LineSegment2D
 to2DSegment (p1,p2) = (to2D p1, to2D p2)
 
 
-distance = 50
-zCoordinate = 25
+distance = 150
+zCoordinate = 0
 
 cube :: Object
 cube = [
+     ((-50,-50, zCoordinate-20), (50, -50, zCoordinate-20)),
+     ((50,-50, zCoordinate-20), (50, 50, zCoordinate-20)),
+     ((50,50, zCoordinate-20), (-50, 50, zCoordinate-20)),
+     ((-50,50, zCoordinate-20), (-50, -50, zCoordinate-20)),
+
      ((-50,-50, zCoordinate), (50, -50, zCoordinate)),
      ((50,-50, zCoordinate), (50, 50, zCoordinate)),
      ((50,50, zCoordinate), (-50, 50, zCoordinate)),
      ((-50,50, zCoordinate), (-50, -50, zCoordinate)),
 
-     ((-50,-50, zCoordinate+50), (50, -50, zCoordinate+50)),
-     ((50,-50, zCoordinate+50), (50, 50, zCoordinate+50)),
-     ((50,50, zCoordinate+50), (-50, 50, zCoordinate+50)),
-     ((-50,50, zCoordinate+50), (-50, -50, zCoordinate+50))
+
+     ((-50,-50, zCoordinate+20), (50, -50, zCoordinate+20)),
+     ((50,-50, zCoordinate+20), (50, 50, zCoordinate+20)),
+     ((50,50, zCoordinate+20), (-50, 50, zCoordinate+20)),
+     ((-50,50, zCoordinate+20), (-50, -50, zCoordinate+20))
      ] 
 
 objectToJSFunction :: Object -> String
@@ -65,6 +72,19 @@ getJS objects = "var actionList = [\n" ++ (foldr g [] (map objectToJSFunction ob
         g scr [] = scr
         g scr rest = scr ++ "," ++ rest
 
+rotateObject :: Angle -> Axis -> Object -> Object
+rotateObject angle axis = map f
+             where
+                f (p1,p2) = (rotate p1 angle axis, rotate p2 angle axis)
 
 
+getObjectList :: [Angle] -> [Object]
+getObjectList [] = []
+getObjectList (i:is) = (rotateObject i X cube) : getObjectList is
 
+angles :: [Angle]
+angles = [0..360]
+
+main = do
+  writeFile "actionList.js" (getJS (getObjectList angles))
+  return ()
