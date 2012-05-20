3f73efa576749c064ab9e4be9a69d1909ec2568d
CK Kashyap <ckkashyap@gmail.com>
2012-05-20 10:56:04 +0530

    working version

--- Haskell/HTML5/JavaScript.hs
+++ Haskell/HTML5/JavaScript.hs
@@ -11,22 +11,22 @@ jsFunction j = case j of
 
 point2js :: JSFunction -> Geometry.Point2D -> String
 point2js jsf (x,y) = f ++ "(" ++ (show x) ++ ", " ++ (show y) ++ ");\n"
-         where f = "context." ++ jsFunction jsf
+         where f = "\tctx." ++ jsFunction jsf
 
 triangle2js :: Geometry.Triangle2D -> String
-triangle2js (p1, p2, p3) = foldr (\(_p1, _p2) y -> (s _p1 _p2) ++ y) "" linePointsList
-            where s __p1 __p2      = (point2js MoveTo __p1) ++ (point2js LineTo __p2)
-                  list             = cycle [p1, p2, p3]
-                  listShiftedByOne = tail list
-                  linePointsList   = take 3 (zip list listShiftedByOne)
-                  
-            
+triangle2js (p1, p2, p3) = triangle2js' (take 4 (cycle [p1, p2, p3])) where
+            triangle2js' (_p1:_p2:[]) = str _p1 _p2
+            triangle2js' (_p1:_p2:ps) = (str _p1 _p2) ++ (triangle2js' (_p2:ps))
+            str _p1 _p2               = (point2js MoveTo _p1) ++ (point2js LineTo _p2)
+
 object2JS :: Geometry.Scene -> String
-object2JS object = "function (context) {\n" ++ (object2JS' object2D)  ++ "}\n"
+object2JS object = "function (ctx) {\n" ++ (object2JS' object2D)  ++ "}\n"
  where
   object2D = map Geometry.projectTriangle object
   object2JS' [] = ""
   object2JS' (t:ts) = triangle2js t ++ object2JS' ts
+
+
 
 animation2JS :: Geometry.Animation -> String
 animation2JS animation = "var actionList = [\n" ++ (foldr f [] (map object2JS animation)) ++ "]\n" where
@@ -34,15 +34,12 @@ animation2JS animation = "var actionList = [\n" ++ (foldr f [] (map object2JS an
              f sceneJS rest =  sceneJS ++ ",\n" ++ rest
 
 
-test_points3D = map (\x -> (x,x+1,x-1)) [1,2,3] :: [Geometry.Point3D]
+test_points = map (\i -> (i,i+1,i-1)) [1..3] :: [Geometry.Point3D]
 
-test_triangle3D = (p1,p2,p3) where
-              [p1, p2, p3] = test_points3D
-              
-test_scene = [test_triangle3D]
+test_triangle = (p1, p2, p3) where [p1, p2, p3] = test_points
 
-test_animation = take 2 (repeat test_scene)
+test_scene = [test_triangle] :: Geometry.Scene
 
+test_animation = take 2 $ cycle [test_scene] :: Geometry.Animation
 
-
-test_js = animation2JS test_animation
+output = putStr $ animation2JS test_animation
