6fbc971d9f964c19e33955a348a1fd74f5811e50
CK Kashyap <ckkashyap@gmail.com>
2012-07-21 16:42:48 +0530

    correction based on mailing list

--- Haskell/Parsing/xml_2.hs
+++ Haskell/Parsing/xml_2.hs
@@ -17,7 +17,7 @@ xml = do
 innerXML = xml <|> gettext
 
 openTag :: Parser String
-openTag = try (char '<' *> many (noneOf ">")) <* char '>'
+openTag = try (char '<' *> many1 (noneOf "/>")) <* char '>'
 
 endTag :: String -> Parser String
 endTag str = string "</" *> string str <* char '>'
@@ -28,3 +28,4 @@ h2 = parse xml "" "<a><b>A</b></a>"
 h3 = parse xml "" "<a><b><c></c></b></a>"
 h4 = parse xml "" "<a><b></b><c></c>HELLO</a>"
 h5 = parse xml "" "<a><b></b><c></c>HELLO</a>abcd"
+
