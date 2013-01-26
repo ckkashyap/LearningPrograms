adc8affd2fe25ae673fa5fe021b51e315ca7beee
kashyap <ckkashyap@gmail.com>
2013-01-26 14:37:53 +0530

    another_edsl

---
+++ Haskell/monad/Perl/Perl.hs
@@ -0,0 +1,44 @@
+module Perl where
+import Control.Monad.State
+
+
+type MyType = State String String
+
+emit :: String -> MyType
+emit str = do
+	x <- get
+	let n = x ++ str ++ "\n"
+	put n
+	return n
+	
+
+fetch :: MyType -> String
+fetch something = evalState something ""
+
+
+test3 :: MyType
+test3 = do
+	emit "hello"
+	emit "world"
+	return ""
+
+
+
+test1 :: State Int (Int,Int)
+test1 = do
+            a <- get
+            modify (+1)
+            b <- get
+            return (a,b)
+
+
+test2 :: State String (String,String)
+test2 = do
+            a <- get
+            modify (++"1")
+            b <- get
+            return (a,b)
+
+go1 = evalState test1 0
+go2 = evalState test2 "0" 
+
---
+++ Haskell/monad/Perl/test.hs
@@ -0,0 +1,10 @@
+import Perl
+
+
+hello :: MyType
+hello = do
+	emit "hello\n"
+	emit "world\n"
+
+
+x = fetch hello
