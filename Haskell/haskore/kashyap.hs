9856be831b3710936ad7c1bf8e805a01ef668dff
kashyap <ckkashyap@gmail.com>
2013-01-27 21:31:19 +0530

    drums

--- Haskell/haskore/kashyap.hs
+++ Haskell/haskore/kashyap.hs
@@ -84,3 +84,12 @@ t1 = play (Modify (Instrument Percussion)
        Modify (Phrase [Art (Legato  (11/10))]) cms    ))
 
 
+
+
+
+drums a b = Modify (Instrument Percussion)
+                  (line (map (\p-> Prim $ Note sn (pitch p)) [a..b]))
+t11 a b = play (drums a b)
+
+
+testDrums = t11 25 100
