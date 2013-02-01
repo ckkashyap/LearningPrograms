ee4d9c7af413b698060cd8c1f2b918017a5b7c0b
CK Kashyap <ckkashyap@gmail.com>
2013-02-01 13:14:14 +0530

    working version

--- Haskell/IO/index.hs
+++ Haskell/IO/index.hs
@@ -10,18 +10,21 @@ import Data.Binary.Get
 import Data.Binary.Put
 
 
-block0 :: Put
-block0 = do
+block0 :: ByteString
+block0 = runPut $ do
 	mapM (\_ -> putWord32le 0)  [1 .. 26]
 	return ()
 
 
-read26Offsets :: Get [Int]
-read26Offsets = read26Offsets' 26
-read26Offsets' 0 = return []
-read26Offsets' n = do
+numberOfOffsets = 26
+blockSize = numberOfOffsets * 4
+
+readOffsets :: Get [Int]
+readOffsets = readOffsets' numberOfOffsets
+readOffsets' 0 = return []
+readOffsets' n = do
 	x <- getWord32le
-	xs <- read26Offsets' (n-1)	
+	xs <- readOffsets' (n-1)	
 	return (fromIntegral x : xs)
 	
 
@@ -32,13 +35,28 @@ process = do
 	
 
 
+writeInitialBlock :: Handle -> IO ()
+writeInitialBlock handle = do
+	hPutStr handle block0
+	return ()
+
+
 addWord :: Handle -> String -> IO ()
 addWord handle string = do
+	hSeek handle SeekFromEnd 0
+	pos <- hTell handle
+	if pos == 0 
+		then 
+			writeInitialBlock handle 
+		else 
+			return ()
+	
 	hSeek handle AbsoluteSeek 0
+	bs <- hGetContents handle
 
-	
+	let offsets = runGet readOffsets bs
 
-	
+	putStrLn (show offsets)
 
 	return ()
 	
@@ -52,8 +70,9 @@ main = do
 	addWord handle "ABC"
 	--bs <- readFile file
 	--let x = runGet process bs
-	let bs = runPut block0
-	writeFile file bs
+	--let bs = runPut block0
+	--writeFile file bs
+	--putStrLn (show x)
 
 	return ()
 
