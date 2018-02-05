0b0dcf0f551e3940ffccdbedc224f694cd7990ce
CK Kashyap <ckkashyap@gmail.com>
2018-02-04 20:01:43 -0800

    all good

--- clojurescript/build.clj
+++ clojurescript/build.clj
@@ -1,3 +1,6 @@
 (require 'cljs.build.api)
 
-(cljs.build.api/build "src" {:output-to "out/main.js"})
+(cljs.build.api/build "src"
+                      {:main 'hello-world.core
+                       :optimizations :advanced
+                       :output-to "out/main.js"})
