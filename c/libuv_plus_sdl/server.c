bdc652450d1b014a1ecf76f01e2419b507978ce8
CK Kashyap <ckkashyap@gmail.com>
2023-12-28 13:19:56 -0800

    server

--- c/libuv_plus_sdl/server.c
+++ c/libuv_plus_sdl/server.c
@@ -1,12 +1,13 @@
 #include <uv.h>
 
-uv_loop_t *loop;
-
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
-    buf->base = (char*) malloc(suggested_size);
-    buf->len = suggested_size;
+void on_close(uv_handle_t* handle) {
+    free(handle);
 }
 
+void on_alloc(uv_handle_t* client, size_t suggested_size, uv_buf_t* buf) {
+    buf->base = (char*)malloc(suggested_size);
+    buf->len = suggested_size;
+}
 
 void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
     if (nread > 0) {
@@ -15,14 +16,12 @@ void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
         uv_write_t *write_req = (uv_write_t*)malloc(sizeof(uv_write_t));
 
         uv_buf_t write_buf = uv_buf_init((char*)response, strlen(response));
-        uv_write(write_req, client, &write_buf, 1, NULL);
-
-        uv_close((uv_handle_t*)client, NULL);
+        uv_write(write_req, client, &write_buf, 1, on_close);
     } else if (nread < 0) {
         if (nread != UV_EOF) {
             fprintf(stderr, "Read error: %s\n", uv_strerror(nread));
         }
-        uv_close((uv_handle_t*)client, NULL);
+        uv_close((uv_handle_t*)client, on_close);
     }
 
     // Don't forget to free the buffer
@@ -38,20 +37,18 @@ void on_connection(uv_stream_t *server, int status) {
     }
 
     uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-    uv_tcp_init(loop, client);
+    uv_tcp_init(uv_default_loop(), client);
 
     if (uv_accept(server, (uv_stream_t*)client) == 0) {
-        uv_read_start((uv_stream_t*)client, alloc_buffer, on_read);
+        uv_read_start((uv_stream_t*)client, on_alloc, on_read);
     } else {
-        uv_close((uv_handle_t*)client, NULL);
+        uv_close((uv_handle_t*)client, on_close);
     }
 }
 
 int main() {
-    loop = uv_default_loop();
-
     uv_tcp_t server;
-    uv_tcp_init(loop, &server);
+    uv_tcp_init(uv_default_loop(), &server);
 
     struct sockaddr_in addr;
     uv_ip4_addr("0.0.0.0", 8080, &addr);
@@ -64,6 +61,6 @@ int main() {
         return 1;
     }
 
-    return uv_run(loop, UV_RUN_DEFAULT);
+    return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
 
